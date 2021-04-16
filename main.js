a10a803be84eaa23a6bfb80104adf6eb017482f8
architk2010 <82026449+architk2010@users.noreply.github.com>
2021-04-16 15:01:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ var i = 0;
 function nextslide() {
     document.getElementById("reasontext").innerHTML = reasons[i];
     document.getElementById("album").src = images[i];
-    i++;
-    document.getElementById("audio").play();
+    
 
 }
